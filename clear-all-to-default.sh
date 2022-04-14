0a8ec29f22a7f5bfb507f6a43b44283399ddd3d9
aluera <64332771+aluera@users.noreply.github.com>
2022-04-14 11:08:49 +0400

    Update clear-all-to-default.sh

--- clear-all-to-default.sh
+++ clear-all-to-default.sh
@@ -1,17 +1,9 @@
 #!/bin/sh
 echo "Cleaning all to default state..."
 rm -rf ./okd-ignition/
-rm -rf ./secrets/
-rm -r ./terraform/.terraform
+rm -rf ./terraform/.terraform
 rm ./terraform/.terraform.lock.hcl
 rm ./terraform/terraform.tfstate
 rm ./terraform/terraform.tfstate.backup
 rm -rf __pycache__
-FILE=./terraform/main.tf.without
 rm ./terraform/temp_hosts.txt
-if [ -f "$FILE" ]; then
-    echo "$FILE exists."
-else 
-    mv ./terraform/main.tf ./terraform/main.tf.without
-    mv ./terraform/main.tf.save ./terraform/main.tf
-fi
